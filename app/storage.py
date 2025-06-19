ba2311aaff30d7ba73156a569474e7674b242b87
Glen Hayoge <glenhayoge@gmail.com>
2025-06-19 15:42:38 +1000

    feat:added storage

---
+++ app/storage.py
@@ -0,0 +1,12 @@
+import json
+import os
+
+DATA_FILE = os.path.join(os.path.dirname(__file__), "../data/a_complete.json")
+
+def load_data():
+    with open(DATA_FILE, "r", encoding="utf-8") as f:
+        return json.load(f)
+
+def save_data(data):
+    with open(DATA_FILE, "w", encoding="utf-8") as f:
+        json.dump(data, f, ensure_ascii=False, indent=2)
